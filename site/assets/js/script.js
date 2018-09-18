293589889672ceeb4ecd224598c5fdb84f11fdcf
gabriel80546 <gabriel80546@gmail.com>
2018-09-18 11:36:51 -0300

    asdfasdfijwer

--- site/assets/js/script.js
+++ site/assets/js/script.js
@@ -22,8 +22,9 @@ function onLoad() {
 	for(var i = 0; i < lenguePreco; i++) {
 		loadDoc(i+1, 'PRECO_ATACADO', mepega[i]);
 	}
-	//gerarCard(img, preco, nome, cor);
-	gerarCard();
+	//gerarCard(img, cor, nome, preco);
+	gerarCard("assets/img/db/04.jpg", "AZUL", "IPANEMA", "12.5");
+	gerarCard("assets/img/db/05.jpg", "ROSA", "IPANEMA", "12.5");
 }
 function loadDoc(id, coluna, cursor) {
 	var xhttp = new XMLHttpRequest();
@@ -87,12 +88,9 @@ function level2 (next) {
 }
 
 
-function gerarCard() {
+function gerarCard(imgCaminho, cor, nome, preco) {
 	var card = document.querySelector('novocard');
 	var cardWarper = card.parentElement;
-	console.log("cardWarper: ");
-	console.log(cardWarper);
-	cardWarper.removeChild(card);
 
 	var div1 = document.createElement("div");
 	div1.setAttribute("class", "col-lg-3 col-md-6 mb-4");
@@ -106,10 +104,58 @@ function gerarCard() {
 	div3.setAttribute("class", "view overlay");
 	div2.insertAdjacentElement('beforeend', div3);
 
-
+	// IMAGEM
 	var img = document.createElement("img");
-	img.setAttribute("src", "assets/img/db/04.jpg");
+	img.setAttribute("src", imgCaminho);
 	img.setAttribute("class", "card-img-top");
 	div3.insertAdjacentElement('beforeend', img);
 
+	var a1 = document.createElement("a");
+	div3.insertAdjacentElement('beforeend', a1);
+
+	var div4 = document.createElement("div");
+	div4.setAttribute("class", "mask rgba-white-slight waves-effect waves-light");
+	a1.insertAdjacentElement('beforeend', div4);
+
+	var div5 = document.createElement("div");
+	div5.setAttribute("class", "card-body text-center");
+	div2.insertAdjacentElement('beforeend', div5);
+
+	var a2 = document.createElement("a");
+	a2.setAttribute("class", "grey-text");
+	div5.insertAdjacentElement('beforeend', a2);
+
+	// COR
+	var h5 = document.createElement("h5");
+	h5.innerHTML = cor;
+	a2.insertAdjacentElement('beforeend', h5);
+
+	var h5a = document.createElement("h5");
+	div5.insertAdjacentElement('beforeend', h5a);
+
+	var strong = document.createElement("strong");
+	h5a.insertAdjacentElement('beforeend', strong);
+
+	// NOME
+	var a3 = document.createElement("a");
+	a3.setAttribute("class", "dark-grey-text");
+	a3.innerHTML = nome;
+	strong.insertAdjacentElement('beforeend', a3);
+
+	var h4 = document.createElement("h4");
+	h4.setAttribute("class", "font-weight-bold blue-text");
+	div5.insertAdjacentElement('beforeend', h4);
+
+	var strong1 = document.createElement("strong");
+	strong1.setAttribute("data-toggle", "tooltip");
+	strong1.setAttribute("data-placement", "left");
+	strong1.setAttribute("title", "Atacado");
+	var n = new Number(preco);
+	strong1.innerHTML = "R$ " + n.toPrecision(Math.floor(Math.log10(n)) + 3).toString().replace("\.", ",");
+	h4.insertAdjacentElement('beforeend', strong1);
+
+	$(function () {
+		$('[data-toggle="tooltip"]').tooltip()
+	})
+	//cardWarper.removeChild(card);
 }
