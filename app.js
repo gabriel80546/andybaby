64007b6681bc7be436a8730741af24e1056d628c
gabriel80546 <gabriel80546@gmail.com>
2018-09-23 18:26:00 +0200

    asdfasdfijwer

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use('/', function(req, res, next) {
 
 
 var server = app.listen(80, function () {
-	var host = server.address().address
+	var addr = server.address().address
 	var port = server.address().port
-	console.log("rodando em http://%s:%s", host, port)
+	console.log("rodando em http://%s:%s", addr, port)
 });
